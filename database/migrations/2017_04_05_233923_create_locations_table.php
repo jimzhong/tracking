2e3d4d0958b55dbd50a747c84c3c03406effe360
Jim Zhong <zhonghc@staff.zjutv.com>
2017-04-10 09:40:37 +0800

    add heading

--- database/migrations/2017_04_05_233923_create_locations_table.php
+++ database/migrations/2017_04_05_233923_create_locations_table.php
@@ -17,7 +17,8 @@ class CreateLocationsTable extends Migration
             $table->increments('id');
             $table->float('lat', 10, 6);
             $table->float('lng', 10, 6);
-            $table->float('speed', 6, 3)->nullable();    // km/h
+            $table->float('speed')->nullable();
+            $table->float('heading')->nullable();
             $table->integer('battery')->nullable(); // 0-100
             $table->integer('device_id')->unsigned();
             $table->timestamp('sampled_at');
